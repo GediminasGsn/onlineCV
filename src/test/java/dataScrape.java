3a684f116ec142ef51e57ec8c2b42a02923bf068
GediminasGsn <gasiunasgediminas@gmail.com>
2024-04-15 12:33:05 +0300

    **

--- src/test/java/dataScrape.java
+++ src/test/java/dataScrape.java
@@ -1,6 +1,9 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class dataScrape {
 
@@ -12,6 +15,7 @@ public class dataScrape {
         driver.manage().window().maximize();
 
     }
+
     @Test
     public void scrape() {
         ChromeDriver driver = new ChromeDriver();
@@ -27,11 +31,32 @@ public class dataScrape {
             driver.get(url1);
             if (i == 2) {
                 continue;
+            }
                 if (!driver.getCurrentUrl().equals(url1)) {
                     break; //end loop
                 }
+
                 //Wait
                 try {
                     Thread.sleep(5000); // Wait for 5 seconds
                 } catch (InterruptedException e) {
                     e.printStackTrace();
+                }
+                //Listing all adds
+                List<WebElement> h3Elements = driver.findElements(By.xpath("//*[@id=\"__next\"]/div[2]/div[2]/div/div[2]/div/div/ul/li[1]"));
+
+                //Run it through condition
+                for (WebElement h3 : h3Elements) {
+                    String h3Text = h3.getText();
+                    if (h3Text.contains("QA") || h3Text.contains("automation") || h3Text.contains("engineer") || h3Text.contains("testuotojas")) {
+                        System.out.println("");
+                        System.out.println("Pozicija: " + h3.findElement(By.xpath("/html/body/div[1]/div[2]/div[2]/div/div[2]/div/div/ul/li[1]/div/a/div[2]/div[1]/div/span[1]")).getText()); // Print position
+                        System.out.println("Darbo uždarbis: " + h3.findElement(By.xpath("/html/body/div[1]/div[2]/div[2]/div/div[2]/div/div/ul/li[1]/div/a/div[2]/div[2]/div[2]/div/span[1]/span")).getText()); //Print salary
+                        System.out.println("Skelbimo URL: " + h3.findElement(By.xpath("/html/body/div[1]/div[2]/div[2]/div/div[2]/div/div/ul/li[1]/div/a")).getAttribute("href")); //Print URL
+                    }
+
+                }
+
+            }
+        }
+    }
